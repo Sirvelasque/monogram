dbb5fbc6995b19cd7fc44426f66a497540f917d1
Walter Velasquez <velasquezwalter10@yahoo.es>
2023-10-08 15:35:43 -0600

    create temporal items list

---
+++ src/redux/shopItem.jsx/testItems.js
@@ -0,0 +1,31 @@
+const items = [
+  {
+    name: "green",
+    price: 10,
+    description: [
+      "green",
+      "small"
+    ],
+    inStock: true
+  },
+  {
+    name: "red",
+    price: 20,
+    description: [
+      "red",
+      "big"
+    ],
+    inStock: false
+  },
+  {
+    name: "blue",
+    price: 20,
+    description: [
+      "dlue",
+      "big"
+    ],
+    inStock: true
+  }
+];
+
+export default items;
