09bd130cb2dcd65bba4aa98455b6d8be3e6aacac
Walter Velasquez <velasquezwalter10@yahoo.es>
2023-10-08 15:23:08 -0600

    Install react dependency and create Item constructor

---
+++ src/redux/shopItem.jsx/item.js
@@ -0,0 +1,10 @@
+class Item {
+  constructor(name, description, price, inStock) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.inStock = inStock;
+  }
+}
+
+export default Item;
